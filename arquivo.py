9863ba7acc21b16fb5f2dd0cfdd5a3c3dab5b0a7
Dionisio <105153876+dionisiosp@users.noreply.github.com>
2023-05-22 22:00:25 -0300

    versao1

--- arquivo.py
+++ arquivo.py
@@ -2,3 +2,34 @@
 # RM: 97985
 
 print("Insira as toneladas colhidas por mês:")
+print("")
+print("")
+
+mes = 1
+tmanual = 0
+tmaquina = 0
+colheita = 0
+
+while mes <= 12:
+    #tonelada = input("Mes "  " ....: ")
+    tonelada = input()
+    print("Mês", mes, "....:",tonelada)
+    manual = int(tonelada) * 0.05
+    maquina = int(tonelada) * 0.15
+    print(f'     Perda manual..........: {manual:,.2f}')
+    print(f'     Perda com máquina.....: {maquina:,.2f}')
+    print("")
+    mes = mes + 1
+    tmanual = tmanual + manual
+    tmaquina = tmaquina + maquina
+    colheita = colheita + int(tonelada)
+
+print("")
+print("RELATÓRIO CONSOLIDADO:")
+print("")
+print("")
+
+print(f'Colheita do ano.....: {colheita:,.2f} Toneladas')
+print("Projeção de desperdício:")
+print(f'  Manual..........: {tmanual:,.2f}')
+print(f'  Com máquina.....: {tmaquina:,.2f}')
